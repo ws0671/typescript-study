ca6343b5ad7197a1cde4585089560bbe6dcbdb96
Yeongmin Lee <ws0671@naver.com>
2026-05-25 20:53:10 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React, { Suspense } from "react";
+import "./App.css";
+import { Route, Routes } from "react-router";
+const AppLayout = React.lazy(() => import("./layout/AppLayout"));
+const HomePage = React.lazy(() => import("./pages/HomePage/HomePage"));
+const SearchPage = React.lazy(() => import("./pages/SearchPage/SearchPage"));
+
+function App() {
+  return (
+    <Suspense fallback={<div>loading...</div>}>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/search" element={<SearchPage />} />
+          {/* <Route path="/search/:keyword" element={<SearchWithKeywordPage />} />
+      <Route path="/playlist/:id" element={<PlaylistDetailPage />} />
+      <Route path="/playlist" element={<PlaylistPage />} /> */}
+        </Route>
+      </Routes>
+    </Suspense>
+  );
+}
+
+export default App;
